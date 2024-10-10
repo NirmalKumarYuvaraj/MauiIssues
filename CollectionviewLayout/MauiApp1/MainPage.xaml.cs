91c52ec992bd96c69295449b9746851cb63cffff
NirmalKumarYuvaraj <97871636+NirmalKumarYuvaraj@users.noreply.github.com>
2024-10-10 18:43:05 +0530

    Add files via upload

---
+++ CollectionviewLayout/MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿
+namespace MauiApp1
+{
+	public partial class MainPage : ContentPage
+	{
+		public List<Item> ItemCollection { get; set; }
+
+		public MainPage()
+		{
+			InitializeComponent();
+            ItemCollection = new List<Item>();
+            ItemCollection.Add(new Item() { ItemName = "Item1" });
+            //ItemCollection.Add(new Item() { ItemName = "Item2"});
+            colView.ItemsSource = ItemCollection;
+			BindingContext = this;
+
+		}
+
+		public class Item
+		{
+			public string? ItemName { get; set; }
+		}
+	}
+}

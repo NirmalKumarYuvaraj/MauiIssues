877c01eb836cd0833a6d98b2805eec93d9e1ea69
NirmalKumarYuvaraj <97871636+NirmalKumarYuvaraj@users.noreply.github.com>
2024-12-26 12:13:48 +0530

    Added ListView uneven scroliing issue sample

---
+++ ListviewUnevenRowScrolling/ListviewUnevenRowScrolling/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ListviewUnevenRowScrolling;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ ListviewUnevenRowScrolling/ListviewUnevenRowScrolling/MainPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.ObjectModel;
+
+namespace ListviewUnevenRowScrolling;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+		Init();
+	}
+
+	ListView list;
+	StackLayout stackLayout;
+
+	ObservableCollection<Person1> Data;
+	public static int Count = 0;
+
+
+	void Init()
+	{
+		list = new ListView();
+
+		var template = new DataTemplate(typeof(UnevenViewCell));
+		list.ItemTemplate = template;
+
+		Data = new ObservableCollection<Person1>();
+		list.ItemsSource = Data;
+		list.HasUnevenRows = true;
+
+		for (int i = 0; i < 50; i++)
+		{
+			Data.Add(new Person1
+			{
+				FullName = "Andrew",
+				Address = "404 Somewhere"
+			});
+		}
+
+
+
+		stackLayout = new StackLayout();
+		stackLayout.Children.Add(list);
+
+		this.Content = stackLayout;
+	}
+
+}
+class UnevenViewCell : ViewCell
+{
+	public UnevenViewCell()
+	{
+
+		var label = new Label();
+		label.SetBinding(Label.TextProperty, "FullName");
+		Height = MainPage.Count % 2 == 0 ? 50 : 100;
+		View = label;
+		View.BackgroundColor = MainPage.Count % 2 == 0 ? Colors.Pink : Colors.LightYellow;
+		MainPage.Count++;
+	}
+}
+
+
+class Person1
+{
+	public string FullName { get; set; }
+	public string Address { get; set; }
+}
+
+
+
+
+
+
+

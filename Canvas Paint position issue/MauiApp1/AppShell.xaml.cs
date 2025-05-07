3083f32b2f98668d22f04cee11ad454dbf1a7f48
NirmalKumarYuvaraj <97871636+NirmalKumarYuvaraj@users.noreply.github.com>
2025-05-07 13:27:37 +0530

    Add files via upload

---
+++ Canvas Paint position issue/MauiApp1/AppShell.xaml.cs
@@ -0,0 +1,82 @@
+﻿namespace MauiApp1;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+		Routing.RegisterRoute("MainPage", typeof(MainPage));
+	}
+}
+
+public static class HandlerDisconnectBehavior
+{
+	public static readonly BindableProperty CascadeProperty =
+		BindableProperty.CreateAttached("Cascade", typeof(bool), typeof(HandlerDisconnectBehavior), false,
+			propertyChanged: CascadeChanged);
+
+	public static readonly BindableProperty SuppressProperty =
+		BindableProperty.CreateAttached("Suppress", typeof(bool), typeof(HandlerDisconnectBehavior), false);
+
+	public static bool GetCascade(BindableObject view)
+	{
+		return (bool)view.GetValue(CascadeProperty);
+	}
+
+	public static void SetCascade(BindableObject view, bool value)
+	{
+		view.SetValue(CascadeProperty, value);
+	}
+
+	public static bool GetSuppress(BindableObject view)
+	{
+		return (bool)view.GetValue(SuppressProperty);
+	}
+
+	public static void SetSuppress(BindableObject view, bool value)
+	{
+		view.SetValue(SuppressProperty, value);
+	}
+
+	private static void CascadeChanged(BindableObject view, object oldValue, object newValue)
+	{
+		if (view is not VisualElement visualElement)
+			throw new InvalidOperationException(
+				"HandlerDisconnectBehavior.Cascade can only be attached to a VisualElement");
+
+		var attachBehavior = (bool)newValue;
+		if (attachBehavior)
+			visualElement.Unloaded += OnVisualElementUnloaded;
+		else
+			visualElement.Unloaded -= OnVisualElementUnloaded;
+	}
+
+	private static void OnVisualElementUnloaded(object? sender, EventArgs e)
+	{
+		if (sender is not VisualElement senderElement)
+			return;
+
+		if (GetSuppress(senderElement))
+			return;
+
+		var visualTreeElement = (IVisualTreeElement)senderElement;
+
+		Disconnect(visualTreeElement);
+
+		return;
+
+		void Disconnect(IVisualTreeElement element)
+		{
+			if (element is VisualElement visualElement)
+			{
+				if (GetSuppress(visualElement))
+					return;
+
+				foreach (IVisualTreeElement childElement in element.GetVisualChildren())
+					Disconnect(childElement);
+
+				visualElement.Handler?.DisconnectHandler();
+			}
+		}
+	}
+}
---
+++ Canvas Paint position issue/MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿
+namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	GraphicsView _graphicsView;
+	PaintDrawable _drawable;
+
+	public MainPage()
+	{
+		InitializeComponent();
+
+		var rootLayout = new VerticalStackLayout() { BackgroundColor = Colors.Red };
+
+		_graphicsView = new GraphicsView()
+		{
+			HeightRequest = 300,
+			WidthRequest = 300
+		};
+		_drawable = new PaintDrawable();
+		_graphicsView.Drawable = _drawable;
+		rootLayout.Add(_graphicsView);
+		Content = rootLayout;
+	}
+}
+
+public class PaintDrawable : IDrawable
+{
+
+	public void Draw(ICanvas canvas, RectF dirtyRect)
+	{
+		IPattern pattern;
+
+		// Create a 10x10 template for the pattern
+		using (PictureCanvas picture = new PictureCanvas(0, 0, 10, 10))
+		{
+			picture.StrokeColor = Colors.Silver;
+			picture.DrawLine(0, 0, 10, 10);
+			picture.DrawLine(0, 10, 10, 0);
+			pattern = new PicturePattern(picture.Picture, 10, 10);
+		}
+
+		// Fill the rectangle with the 10x10 pattern
+		PatternPaint patternPaint = new PatternPaint
+		{
+			Pattern = pattern
+		};
+		canvas.SetFillPaint(patternPaint, RectF.Zero);
+		canvas.FillRectangle(0, 0, 250, 250);
+
+	}
+}
